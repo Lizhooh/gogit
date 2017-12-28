39181f8bbbbbf9577ba99c74abd4166d1b6b1793
lizhooh <960316408@qq.com>
2017-12-28 17:32:20 +0800

    update：version to v1.0.1

--- src/main/index.js
+++ src/main/index.js
@@ -16,58 +16,59 @@ if (process.env.NODE_ENV !== 'development') {
     global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
-const STATIC = (p) => path.join(__dirname, '../../static', p);
+const STATIC = (p) => path.join(__static, p);
 
-let mainWindow;
+let win;
 let appIcon;
 const winURL = process.env.NODE_ENV === 'development'
     ? `http://localhost:9080`
     : `file://${__dirname}/index.html`
 
+const isDev = process.env.NODE_ENV === 'development';
+
 function createWindow() {
     /**
      * Initial window options
      */
-    mainWindow = new BrowserWindow({
+    win = new BrowserWindow({
         height: 480,
-        width: 320 + 400,
+        width: 320,
+        maxWidth: 320,
+        maxHeight: 480,
+        resizable: false,
         useContentSize: true,
         frame: false,                   // 边框
         transparent: true,              // 背景透明
-        icon: STATIC('icons/icon.ico'),
+        show: false,
         title: 'GoGit 服务管理工具',
     });
 
-    mainWindow.loadURL(winURL);
-    mainWindow.setMenu(null);
+    const logo = STATIC('logo.png');
 
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    });
+    win.loadURL(winURL);
+    win.setMenu(null);
+    win.setIcon(logo);
+    win.setOverlayIcon(logo, 'GoGit 服务管理工具');
+    win.setThumbnailToolTip('GoGit 服务管理工具');
+
+    win.on('closed', () => win = null);
+    win.once('ready-to-show', () => win.show());
 
     // 托盘图标
-    appIcon = new Tray(STATIC('icons/icon.ico'));
+    appIcon = new Tray(logo);
     appIcon.setToolTip('GoGit 服务管理工具');
-    appIcon.on('click', event => {
-        // 显示窗口并获取焦点
-        mainWindow.show();
-    });
+    appIcon.on('click', event => win.show());
     appIcon.setContextMenu(Menu.buildFromTemplate([
-        {
-            label: '打开', type: 'normal', click() {
-                mainWindow.show();
-            }
-        },
-        {
-            label: '退出', type: 'normal', role: 'close', click() {
-                mainWindow.close();
-            }
-        },
+        { label: '打开', type: 'normal', click() { win.show() } },
+        { label: '退出', type: 'normal', role: 'close', click() { win.close() } },
     ]));
 }
 
-app.on('ready', createWindow);
+app.setJumpList(null);
+app.clearRecentDocuments();
+app.setAppUserModelId('GoGit');
 
+app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
@@ -75,13 +76,24 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    if (mainWindow === null) {
+    if (win === null) {
         createWindow();
     }
 });
 
 // 窗口隐藏
 ipcMain.on('#window-hide', () => {
-    mainWindow.hide();
+    win.hide();
 });
 
+// 保证只运行一个窗口
+const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+    if (win) {
+        if (win.isMinimized()) win.restore();
+        win.focus();
+    }
+});
+
+if (shouldQuit) {
+    app.quit();
+}
